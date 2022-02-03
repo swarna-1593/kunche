6d72822f11fecfbe757b32968688aaf6e2790f00
swarna-1593 <70028269+swarna-1593@users.noreply.github.com>
2022-02-03 10:36:19 +0530

    Add files via upload

---
+++ 1.Sum of N numbers.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace DemoCapgemini
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+           int i,j,r;
+	
+     Console.Write("Display the pattern like right angle using asterisk:\n");
+    Console.Write("------------------------------------------------------");
+    Console.Write("\n\n");   
+   
+   Console.Write("Input: ");
+   r= int.Parse(Console.ReadLine());   
+   for(i=1;i<=rows;i++)
+   {
+	for(j=1;j<=i;j++)
+    {
+	   Console.Write("*");
+    }
+	Console.Write("\n");
+   }
+  }   
+}
+
+        }
+    
+
+
